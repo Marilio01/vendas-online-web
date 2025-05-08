1fc87f9b8cda6ad23bbd404351dee310aced850b
marilio01 <mariliocavalcanti16@gmail.com>
2025-05-08 12:14:43 -0300

    feat: insert token in localStorage

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -11,6 +11,7 @@ import {
   LimitedContainer,
   TitleLogin,
 } from '../styles/loginScreen.styles';
+import { UserType } from '../types/UserType';
 
 const LoginScreen = () => {
   const { accessToken, setAccessToken } = useGlobalContext();
@@ -26,12 +27,13 @@ const LoginScreen = () => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    setAccessToken('novo token');
-    postRequest('http://localhost:8080/auth', {
+  const handleLogin = async () => {
+    const user = await postRequest<UserType>('http://localhost:8080/auth', {
       email: email,
       password: password,
     });
+
+    setAccessToken(user?.accessToken || '');
   };
 
   return (
