dd95fd2be57d099fc8db43d0526680e59211e1d2
marilio01 <mariliocavalcanti16@gmail.com>
2025-05-13 18:00:08 -0300

    feat: insert token in request

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -2,9 +2,16 @@ import axios from 'axios';
 import { useState } from 'react';
 import { connectionAPIPost } from '../functions/connection/connectionAPI';
 import { useGlobalContext } from './useGlobalContext';
+import { useNavigate } from 'react-router-dom';
+import { AuthType } from '../../modules/login/types/AuthType';
+import { ProductRoutesEnum } from '../../modules/product/routes';
+import { ERROR_INVALID_PASSWORD } from '../constants/errosStatus';
+import { URL_AUTH } from '../constants/urls';
+import { setAuthorizationToken } from '../functions/connection/auth';
 
 export const useRequests = () => {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const { setNotification } = useGlobalContext();
 
   const getRequest = async (url: string) => {
@@ -23,9 +30,9 @@ export const useRequests = () => {
 
   const postRequest = async <T>(url: string, body: unknown): Promise<T | undefined> => {
     setLoading(true);
+
     const returnData = await connectionAPIPost<T>(url, body)
       .then((result) => {
-        setNotification('Entrando...', 'success');
         return result;
       })
       .catch((error: Error) => {
@@ -37,8 +44,26 @@ export const useRequests = () => {
     return returnData;
   };
 
+  const authRequest = async (body: unknown): Promise<void> => {
+    setLoading(true);
+
+    await connectionAPIPost<AuthType>(URL_AUTH, body)
+      .then((result) => {
+        setAuthorizationToken(result.accessToken);
+        navigate(ProductRoutesEnum.PRODUCT);
+        return result;
+      })
+      .catch(() => {
+        setNotification(ERROR_INVALID_PASSWORD, 'error');
+        return undefined;
+      });
+
+    setLoading(false);
+  };
+
   return {
     loading,
+    authRequest,
     getRequest,
     postRequest,
   };
