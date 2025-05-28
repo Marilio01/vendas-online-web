6318a4361c336d5edbf37165052cdc756c5cba2b
João Victor <joaobarbosavictor132@gmail.com>
2025-05-28 13:51:28 -0300

    feat: add new routes to urls.ts

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -1,8 +1,14 @@
 export const URL_USER = 'http://localhost:8080/user';
 
+export const URL_USER_ADMIN = 'http://localhost:8080/user/admin';
+
 export const URL_USER_ID = 'http://localhost:8080/user/{userId}';
 
 export const URL_USER_ALL = 'http://localhost:8080/user/all';
+
+export const URL_USER_ALL_ADMIN = 'http://localhost:8080/user/admins';
+
+export const URL_USER_ALL_COMUM = 'http://localhost:8080/user/users';
 
 export const URL_AUTH = 'http://localhost:8080/auth';
 
@@ -11,6 +17,7 @@ export const URL_PRODUCT = 'http://localhost:8080/product';
 export const URL_PRODUCT_ID = 'http://localhost:8080/product/{productId}';
 
 export const URL_CATEGORY = 'http://localhost:8080/category';
+
 export const URL_CATEGORY_ID = 'http://localhost:8080/category/{categoryId}';
 
 export const URL_ORDER_ID = 'http://localhost:8080/order/{orderId}';
