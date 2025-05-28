f16c77e2bcb9d0a39f278bcfb595e5d48c0a7670
João Victor <joaobarbosavictor132@gmail.com>
2025-05-28 14:16:19 -0300

    feat: add user type logic to FirstScreen.tsx

--- src/modules/firstScreen/screens/FirstScreen.tsx
+++ src/modules/firstScreen/screens/FirstScreen.tsx
@@ -1,20 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useGlobalReducer } from '../../../store/reducers/globalReducer/useGlobalReducer';
 import { ProductRoutesEnum } from '../../product/routes';
 import Loading from '../../../shared/components/loading/Loading';
+import { LoginRoutesEnum } from '../../login/routes';
+import { UsuarioDisplayRoutesEnum } from '../../usuarioDisplay/routes';
+import { UserTypeEnum } from '../../../shared/enums/userType.enum';
+import { getUserInfoByToken } from '../../../shared/functions/connection/auth';
 
 const FirstScreen = () => {
-  const { user } = useGlobalReducer();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user) {
-      navigate(ProductRoutesEnum.PRODUCT);
-    }
-  }, [user]);
+  const userToken = useMemo(() => getUserInfoByToken(), []);
 
-   return <Loading />;
+  useEffect(() => {
+    if (userToken) {
+      if (userToken.typeUser === UserTypeEnum.Root || userToken.typeUser === UserTypeEnum.Admin) {
+        navigate(ProductRoutesEnum.PRODUCT);
+      } else if (userToken.typeUser === UserTypeEnum.User) {
+        navigate(UsuarioDisplayRoutesEnum.USUARIO_DISPLAY);
+      } else {
+        navigate(LoginRoutesEnum.LOGIN);
+      }
+    } else {
+      navigate(LoginRoutesEnum.LOGIN);
+    }
+  }, [userToken, navigate]);
+
+  return <Loading />;
 };
 
 export default FirstScreen;
