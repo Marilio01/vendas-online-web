eaca639856068dd97dfcc64b8836de971bbb68e8
João Victor <joaobarbosavictor132@gmail.com>
2025-06-13 11:58:22 -0300

    refactor: update firstScreen component structure and logic

--- src/modules/firstScreen/screens/FirstScreen.tsx
+++ src/modules/firstScreen/screens/FirstScreen.tsx
@@ -1,30 +1,52 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProductRoutesEnum } from '../../product/routes';
-import Loading from '../../../shared/components/loading/Loading';
 import { LoginRoutesEnum } from '../../login/routes';
 import { UsuarioDisplayRoutesEnum } from '../../usuarioDisplay/routes';
 import { UserTypeEnum } from '../../../shared/enums/userType.enum';
-import { getUserInfoByToken } from '../../../shared/functions/connection/auth';
+import { getAuthorizationToken, getUserInfoByToken } from '../../../shared/functions/connection/auth';
+import { connectionAPIGet } from '../../../shared/functions/connection/connectionAPI';
+import { URL_USER } from '../../../shared/constants/urls';
+import Loading from '../../../shared/components/loading/Loading';
 
 const FirstScreen = () => {
   const navigate = useNavigate();
 
-  const userToken = useMemo(() => getUserInfoByToken(), []);
-
   useEffect(() => {
-    if (userToken) {
-      if (userToken.typeUser === UserTypeEnum.Root || userToken.typeUser === UserTypeEnum.Admin) {
-        navigate(ProductRoutesEnum.PRODUCT);
-      } else if (userToken.typeUser === UserTypeEnum.User) {
-        navigate(UsuarioDisplayRoutesEnum.USUARIO_DISPLAY);
-      } else {
+    (async () => {
+      const token = getAuthorizationToken();
+
+      if (!token) {
+        navigate(LoginRoutesEnum.LOGIN);
+        return; 
+      }
+
+      try {
+        await connectionAPIGet(URL_USER);
+
+        const userInfo = getUserInfoByToken();
+
+        if (!userInfo) {
+            navigate(LoginRoutesEnum.LOGIN);
+            return;
+        }
+
+        if (userInfo.typeUser === UserTypeEnum.Root || userInfo.typeUser === UserTypeEnum.Admin) {
+          navigate(ProductRoutesEnum.PRODUCT);
+        } else if (userInfo.typeUser === UserTypeEnum.User) {
+          navigate(UsuarioDisplayRoutesEnum.USUARIO_DISPLAY);
+        } else {
+          navigate(LoginRoutesEnum.LOGIN);
+        }
+
+      } catch (error) {
+        console.error("Falha na validação do token:", error);
+        localStorage.removeItem('AUTH_TOKEN');
         navigate(LoginRoutesEnum.LOGIN);
       }
-    } else {
-      navigate(LoginRoutesEnum.LOGIN);
-    }
-  }, [userToken, navigate]);
+    })();
+
+  }, [navigate]);
 
   return <Loading />;
 };
